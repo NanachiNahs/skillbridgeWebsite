0d86b7ec3513721fb18bdb93eca02e82fc9cd0d2
Ronan Kuek <121704994+NanachiNahs@users.noreply.github.com>
2026-05-07 11:49:49 +0800

    Revert "making sure works on netlify?"
    This reverts commit 8aa6e1b9c10b5a78cd5940b80246eba9f796a5a7.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+
 import tailwindcss from '@tailwindcss/vite';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
   vite: {
     envPrefix: ['VITE_', 'PUBLIC_', 'EXPO_PUBLIC_'],
     plugins: [tailwindcss()]
   }
-});
+});
